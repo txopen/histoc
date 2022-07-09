15d5b4f76133dbc72ee3670b64fbf65c1235618e
Bruno Lima <balima78@gmail.com>
2022-07-09 18:24:12 +0100

    fix: .yml corrected

--- R/histoc.R
+++ R/histoc.R
@@ -1,9 +1,9 @@
-#' histoc: A package for histocompatibility analysis performed on kidney allocation systems for transplantation
+#' histoc: Histocompatibility analysis performed in kidney allocation systems for transplantation.
 #'
-#' The histoc package provides functions for compatibility between organ donors and transplant candidates.
+#' The \code{histoc} package provides functions for compatibility between organ donors and transplant candidates.
 #'
 #' @section histoc functions:
-#' The histoc main functions: lima, pts, uk, et, several
+#' The \code{histoc} main functions: \code{lima}, \code{pts}, \code{uk}, \code{et}, \code{several}
 #' @import data.table
 #' @docType package
 #' @name histoc
