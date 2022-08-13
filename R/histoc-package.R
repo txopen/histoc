eda6d0a4427c9a20522b5dba6a89f8b058a63b05
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2022-08-13 17:35:00 +0100

    Fixed documentation

--- R/histoc-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom mockr local_mock
-#' @importFrom tibble tribble
-#' @importFrom utils globalVariables
-## usethis namespace: end
-NULL
